1b0ef92372aa5ca3a3359ee0ba6a083c88dbc01f
TheFruitcode <falco.rensner@gmx.de>
2024-01-14 00:15:18 +0100

    Movement funktioniert

--- main.py
+++ main.py
@@ -13,8 +13,18 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Adventure of Thorsten")
 clock = pygame.time.Clock()
 
+#Player
+player_img = pygame.image.load('Character.png')
+player_img_size = pygame.transform.scale(player_img, (200, 325))
+player_img2 = pygame.image.load('Character2.png')
+player_img_size2 = pygame.transform.scale(player_img2, (200, 325))
+player_img3 = pygame.image.load('Character3.png')
+player_img_size3 = pygame.transform.scale(player_img3, (200, 325))
+player_img4 = pygame.image.load('Character4.png')
+player_img_size4 = pygame.transform.scale(player_img4, (200, 325))
+
 #background
-bg = pygame.transform.scale(pygame.image.load('grass.png').convert(), (WIDTH, HEIGHT)) 
+bg = pygame.transform.scale(pygame.image.load('grass.png').convert(), (WIDTH, HEIGHT))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -30,12 +40,16 @@ class Player(pygame.sprite.Sprite):
     
         if keys[pygame.K_w]:
             self.velocity_Y = -self.speed
+            screen.blit(player_img_size4, player.pos)
         if keys[pygame.K_s]:
             self.velocity_Y = self.speed
+            screen.blit(player_img_size, player.pos)
         if keys[pygame.K_a]:
             self.velocity_X = -self.speed
+            screen.blit(player_img_size2, player.pos)
         if keys[pygame.K_d]:
             self.velocity_X = self.speed
+            screen.blit(player_img_size3, player.pos)
     
     def move(self):
         self.pos += pygame.math.Vector2(self.velocity_X, self.velocity_Y)
--- settings.py
+++ settings.py
@@ -4,8 +4,6 @@ WIDTH = 800
 HEIGHT = 600
 
 #Player
-player_img = pygame.image.load('Character.png')
-player_img_size = pygame.transform.scale(player_img, (200, 325))
 playerX = 0
 playerY = 0
 PLAYER_SPEED = 5.5
